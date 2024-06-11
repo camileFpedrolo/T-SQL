7dd5d0f8103bac151cd6a2ff121730ba82ef50f0
Camile Ferreira Pedrolo <143301765+camileFpedrolo@users.noreply.github.com>
2024-06-11 20:02:21 -0300

    Update process_status.sql

--- process_status.sql
+++ process_status.sql
@@ -5,3 +5,10 @@ prc.cmd, prc.nt_domain, prc.nt_username, prc.loginame , text, lastwaittype
 outer apply sys.dm_exec_sql_text(prc.sql_handle)
 where prc.loginame <> ''   and prc.spid > 50 --and status <> 'sleeping'
 order by blocked desc, nt_username desc
+ 
+--Processos em execução
+select prc.spid, prc.blocked, prc.waittime, prc.cpu, prc.login_time, prc.last_batch, prc.status,prc.cmd, prc.nt_domain, 
+prc.nt_username, prc.loginame , text, lastwaittype, hostname, program_name 
+from sys.sysprocesses prc outer apply sys.dm_exec_sql_text(prc.sql_handle)
+where prc.loginame <> '' and prc.spid > 50 and status <> 'sleeping'order by blocked desc, nt_username desc
+has context menu
