4b433c90c15a09beba5854668403617644d56cbc
Camile Ferreira Pedrolo <143301765+camileFpedrolo@users.noreply.github.com>
2024-06-11 19:57:53 -0300

    Create process_status.sql

---
+++ process_status.sql
@@ -0,0 +1,7 @@
+--CONFERIR PROCESSOS EM EXECUÇÃO/PARADOS
+select prc.spid, prc.blocked, prc.waittime, prc.cpu, prc.login_time, prc.last_batch, prc.status,
+prc.cmd, prc.nt_domain, prc.nt_username, prc.loginame , text, lastwaittype
+ from sys.sysprocesses prc
+outer apply sys.dm_exec_sql_text(prc.sql_handle)
+where prc.loginame <> ''   and prc.spid > 50 --and status <> 'sleeping'
+order by blocked desc, nt_username desc
