fcf9c060447f7b396bf1e4859d79f957b8c4dcaf
Camile Ferreira Pedrolo <143301765+camileFpedrolo@users.noreply.github.com>
2024-06-11 19:59:48 -0300

    Create cpu_utilization_SQLServer .sql

---
+++ cpu_utilization_SQLServer .sql
@@ -0,0 +1,23 @@
+--Retrieves CPU use by SQL Server, free, other processes  
+DECLARE @ts_now bigint = (SELECT cpu_ticks/(cpu_ticks/ms_ticks)FROM sys.dm_os_sys_info);  
+ 
+SELECT TOP(30)  
+DATEADD(ms, -1 * (@ts_now - [timestamp]), GETDATE()) AS [Event Time]  , 
+SQLProcessUtilization AS [SQL Server Process CPU Utilization],  
+               SystemIdle AS [System Idle Process],  
+               100 - SystemIdle - SQLProcessUtilization AS [Other Process CPU Utilization]
+ 
+FROM (  
+  SELECT record.value('(./Record/@id)[1]', 'int') AS record_id,  
+record.value('(./Record/SchedulerMonitorEvent/SystemHealth/SystemIdle)[1]', 'int')  
+AS [SystemIdle],  
+record.value('(./Record/SchedulerMonitorEvent/SystemHealth/ProcessUtilization)[1]',  
+'int')  
+AS [SQLProcessUtilization], [timestamp]  
+  FROM (  
+SELECT [timestamp], CONVERT(xml, record) AS [record]  
+FROM sys.dm_os_ring_buffers  
+WHERE ring_buffer_type = N'RING_BUFFER_SCHEDULER_MONITOR'  
+AND record LIKE '%<SystemHealth>%') AS x  
+  ) AS y  
+ORDER BY record_id DESC;
