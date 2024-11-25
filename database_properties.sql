9dd5f1eb08dca9b6a536f0392e23cea73a2fe9ea
Camile Ferreira Pedrolo <143301765+camileFpedrolo@users.noreply.github.com>
2024-11-25 14:43:44 -0300

    Update database_properties.sql

--- database_properties.sql
+++ database_properties.sql
@@ -54,4 +54,13 @@ from
 	inner join sys.databases b on a.database_id = b.database_id
 order by b.state_desc
 
--- rec. model para simple e shrink de log posteriormente 
+-- return database size 
+
+ SELECT d.NAME
+    ,ROUND(SUM(CAST(mf.size AS bigint)) * 8 / 1024, 0) Size_MBs
+    ,(SUM(CAST(mf.size AS bigint)) * 8 / 1024) / 1024 AS Size_GBs
+FROM sys.master_files mf
+INNER JOIN sys.databases d ON d.database_id = mf.database_id
+WHERE d.database_id > 4 -- Skip system databases
+GROUP BY d.NAME
+ORDER BY d.NAME
